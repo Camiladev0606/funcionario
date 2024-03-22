c031833f88f5a42b16df3999b34424d9cb585f10
Camila Lorena Gianini Roda <camila.roda@portalsesisp.org.br>
2024-03-22 10:01:36 -0300

    funcionario e setor

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+using apifuncionario.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace apifuncionario.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext (DbContextOptions options) : base (options)
+
+    {
+    }
+
+    public DbSet<Funcionario> Funcionario {get;set;}
+    public DbSet<Setor> Setor {get;set;}
+}
